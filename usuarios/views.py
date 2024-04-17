8cfd1e7ea987e9521a5d7dad6e4672b852f7d4d2
Paulo0264 <fagundes.dev@hotmail.com>
2024-04-17 15:18:53 -0300

    feat: Finalizan template cadastro

---
+++ usuarios/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            print('Erro 1')
+            return redirect('/usuarios/cadastro')
+
+        if senha != confirmar_senha:
+            print('Erro ')
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            print('Erro 3')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            print('Erro 4')
+            return redirect('/usuarios/cadastro')
